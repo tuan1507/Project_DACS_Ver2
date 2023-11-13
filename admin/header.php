15689d3fbc1c487b705a3a8e2357bb735d898361
tuan1507 <nqtuan.1507@gmail.com>
2023-11-13 19:47:12 +0700

    first commit

---
+++ admin/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="../css/addproduct.css">
+    <link rel="stylesheet" href="../css/home_admin.css">
+    <link rel="stylesheet" href="../css/reponsi.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+
+<body>
+    <!-- header section  -->
+    <header class="header">
+        <div class="header-1">
+            <p>Admin</p>
+            <form action="" class="search-form">
+                <input type="search" name="" id="search-box" placeholder="Search here...">
+                <label for="search-box" class="fas fa-search"></label>
+            </form>
+
+            <div class="icons">
+                <div id="search-btn" class="fas fa-search"></div>
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <div id="login-btn" class="fas fa-user"></div>
+                <div id="login-btn" class="fas fa-heart"></div>
+            </div>
+        </div>
+
+        <div class="header-2">
+            <nav class="navbar">
+                <a href="http://localhost/1.PHP/3.Project/7.DoAnCS2/">Home</a>
+                <a href="#home">Shop</a>
+                <a href="#home">Order</a>
+                <a href="#home">About</a>
+                <a href="#contact">Contact</a>
+            </nav>
+
+        </div>
+
+        <!-- bottom navbar -->
+        <nav class="bottom-navbar">
+            <a href="#home" class="fas fa-home"></a>
+            <a href="#home" class="fas fa-list"></a>
+            <a href="#home" class="fas fa-tags"></a>
+            <a href="#home" class="fas fa-comments"></a>
+            <a href="#contact" class="fas fa-blogs"></a>
+        </nav>
+    </header>
